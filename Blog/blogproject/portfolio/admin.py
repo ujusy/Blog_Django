894421dc0f0a8cefd40ff99ea92bc74a93e29d4d
ujusy <lsyeon1997@ajou.ac.kr>
2020-05-16 03:10:56 +0900

    삭제

--- Blog/blogproject/portfolio/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from portfolio.models import Portfolio
-# Register your models here.
-
-admin.site.register(Portfolio)   
--- Blog/blogproject/portfolio/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from portfolio.models import Portfolio
-# Create your views here.
-def portfolio(request):
-    portfolios = Portfolio.objects
-    return render(request,'portfolio/portfolio.html',{'portfolios':portfolios})
